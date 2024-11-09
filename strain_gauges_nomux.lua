119117c6796dd9994470101930b8da7ac4abf227
TheRad1 <landon.bakken@gmail.com>
2024-11-09 13:41:37 -0600

    performance improvements
    localized a lot of the functions, and just got rid of a lot of unnecessary comments, variables, and other stuff.
    
    Getting ready to attempt to use stream mode

--- strain_gauges_nomux.lua
+++ strain_gauges_nomux.lua
@@ -6,11 +6,14 @@
 -- FILL OUT THESE VARIABLES ACCORDING TO THE STRAIN GAUGES BEING USED
 local exciteVolt = 5      -- External voltage used to excite the SGs (usually 10-12 volts)
 local nominal = 120 -- 120 or 350 ohms
-local elasticModulus = 29000000 --elastic modulus for arms is 29m
+local elasticModulus = 29000000 -- elastic modulus for arms is 29m
 local gaugeFactor = 2.12 --2.12 -- based on the strain gauges
-local logInterval = 50 --in ms
+LJ.IntervalConfig(0, .01) --logging interval in ms (.01 is the fastest it can go without just removing the interval timer)
 
 print("Strain Gauge - Log voltage to file")
+
+--disable truncation warnings (remove if you even slightly suspect issues)
+MB.writeName("LUA_NO_WARN_TRUNCATION", 1)
 
 -- Check for SD card
 if(bit.band(MB.R(60010, 1), 8) ~= 8) then
@@ -21,37 +24,39 @@ end
 -- Initialize local functions (for faster processing)
 local mbRead=MB.R
 local mbWrite = MB.W
+local mbNameToAddress = MB.nameToAddress
 local mbReadName = MB.readName
 local mbWriteName = MB.writeName
-local mbNameToAddress = MB.nameToAddress
+local mbReadArray = MB.RA
+local mbWriteArray = MB.WA
 local checkInterval=LJ.CheckInterval
-local setInterval = LJ.IntervalConfig
 
 -- Initialize variables
-local waitingInterval = 250 --how much time between checks of when loggin should start
-local safeState = 1 -- 1 if file isn't being written to
 local ledState = 1
 
-local newData = 1
 local givenVoltage = 0
-
-local zeroAin = 0
-
 local sgResistance = 0
 local sgResistanceDiff = 0
 local voltageDiff = 0
 local stress = 0
+
 local ainChannel = 0
+
+local logStartTime = 0
+local secondsTable = 0
+
+local filename = ""
+local timetable = 0
 
 local delimiter = ","
 local stressString = ""
 
 -- AIN port config
-local ainChannelCorrection = {0}--, 0, 0, 0, 0} --values that zero each channel
-local ainChannels = {0}--, 4, 6, 8, 10} -- the channels that are read (only even because the odds are the negative channels)
+local ainChannelCorrection = {0, 0, 0, 0, 0}--, 0, 0, 0, 0} --values that zero each channel
+local ainChannels = {0, 0, 0, 0, 0}--, 4, 6, 8, 10} -- the channels that are read (only even because the odds are the negative channels)
 local givenVoltageChannel = 2
 local ainVoltageRange = 0.01 -- +/- 1V input range
-local ainResolution = 10 -- 1 is fastest, 12 is most detail (but slowest)
+local ainResolution = 6 -- 1 is fastest, 12 is most detail (but slowest)
 local ainSettlingTime = 0 -- default settling time
 
 -- functions
@@ -72,22 +77,6 @@ local function configureChannel(channel, range, resolution, settling, differenti
   end
 end
 
-local function updateDebugLED()
-  if(safeState == 1) then
-    ledState = 0 -- stay on
-  else
-    ledState = 1 - ledState -- turn on and off
-  end
-  
-  MB.writeName("FIO1", ledState) -- set lcd
-end
-
-local function setSafeState(newState)
-  safeState = newState -- record state
-  
-  updateDebugLED() -- update led
-end
-
 local function stopProgram(message)
   message = message or "Script was stopped" -- default message
   print(message) --print message
@@ -97,47 +86,6 @@ local function stopProgram(message)
   mbWrite(6000, 1, 0); -- stop
 end
 
-local function getLogFileName()
-  local timetable, error = MB.readNameArray("RTC_TIME_CALENDAR", 6)
-  return string.format(
-    "%04d-%02d-%02d-%02d-%02d-%02d.csv",
-    timetable[1],
-    timetable[2],
-    timetable[3],
-    timetable[4],
-    timetable[5],
-    timetable[6])
-end
-
-local function zeroChannels()
-  setInterval(0, 50)
-  setSafeState(0)
-  
-  print("\nZeroing channels")
-  while zeroAin < 0.5 do
-    if LJ.CheckInterval() then
-      updateDebugLED()
-      
-      --simulating reading the data
-      givenVoltage = mbRead(givenVoltageChannel * 2, 3)
-      for i=1, table.getn(ainChannels) do
-        ain = mbRead(ainChannels[i] * 2, 3) --get value
-      end
-      
-      zeroAin = mbRead(2000, 0) --FIO0
-    end
-  end
-  
-  for i=1, table.getn(ainChannels) do --loop through channels
-    ain = mbRead(ainChannels[i] * 2, 3) --get value
-    
-    ainChannelCorrection[i] = -ain -- set correction to -value
-  end
-  
-  setSafeState(1)
-  print("\nChannel correction:\n", table.concat(ainChannelCorrection, delimiter))
-end
-
 --loop through strain gauge ain channels
 for i=1,table.getn(ainChannels) do
   configureChannel(ainChannels[i], ainVoltageRange, ainResolution, ainSettlingTime, true)
@@ -145,55 +93,60 @@ for i=1,table.getn(ainChannels) do
 end
 configureChannel(givenVoltageChannel, 10, ainResolution, ainSettlingTime, true) --input voltage muesure ain channel
 
+
 while true do --loop forever
-  setSafeState(1) --set default state
-  
-  -- Set waiting interval
-  setInterval(0, waitingInterval)
+  mbWriteName("FIO1", 0) -- set led
   
   print("\nWaiting for button to be pressed")
   while mbRead(2002, 0) >= 0.5 do
-    if checkInterval() then
-
-      --check if user wants to zero channels
-      if mbRead(2000, 0) < 0.5 then
-        zeroChannels()
-      end
-    end
   end
   
+  --get logfile name
+  timetable = MB.readNameArray("RTC_TIME_CALENDAR", 6)
+  filename = string.format(
+    "%04d-%02d-%02d-%02d-%02d-%02d.csv",
+    timetable[1], --year
+    timetable[2], --month
+    timetable[3], --day
+    timetable[4], --hour
+    timetable[5], --minute
+    timetable[6]) --second
+  
   -- Create and open file for write access
-  local Filename = getLogFileName()
-  setSafeState(0) -- set mode to unsafe
-  local file = io.open(Filename, "w")
+  local file = io.open(filename, "w")
 
   -- Make sure that the file was opened properly.
   if file then
-    print("\nCreated and opened debug file with name " .. Filename .. "\n")
+    print("\nCreated and opened debug file with name " .. filename .. "\n")
   else
     -- If the file was not opened properly we probably have a bad SD card.
     stopProgram("!! Failed to open file on uSD Card !! \n Stoping script\n")
   end
-  print("Logging data:")
-  header = "time (seconds), stress (psi)"
-  print(header)
-  file:write(header, "\n") -- Write header to file
   
-  -- Set logging interval
-  setInterval(0, logInterval)
+  --create header
+  header = "time"
+  for i=1,table.getn(ainChannels) do
+    header = string.format("%s, strain%d", header, i)
+  end
+  
+  --write header
+  file:write(header, "\n")
+  print(header)
   
   -- NewData button has been pressed - start recording data
   while mbRead(2002, 0) < 0.5 do
     if checkInterval() then
-      updateDebugLED()
-      stressString = ""--tostring(mbReadName("SYSTEM_COUNTER_10KHZ"))
-      seconds, error = MB.readNameArray("RTC_TIME_S", 2, 1)
-      print(seconds)
+      ledState = 1 - ledState -- turn on and off
+      mbWriteName("FIO1", ledState) -- set led
+      
+      --get time
+      secondsTable = mbReadArray(61500, 0, 2)
+      stressString = string.format("%d%d.%04d", secondsTable[1], secondsTable[2], mbRead(61502, 1))
       
       givenVoltage = mbRead(givenVoltageChannel * 2, 3) -- get Vs
       for i=1,table.getn(ainChannels) do
         -- get variable values
-        voltageDiff = mbRead(0, 3) -- get voltage diff
+        voltageDiff = mbRead(ainChannels[i] * 2, 3) -- get voltage diff
         voltageDiff = voltageDiff + ainChannelCorrection[i] -- correct voltage input
         
         -- math :(
@@ -201,13 +154,12 @@ while true do --loop forever
         sgResistanceDiff = sgResistance - nominal
         stress = sgResistanceDiff/sgResistance*elasticModulus/gaugeFactor
         
-        --print(i .. ": " .. voltageDiff .. "DV, " .. stress .. " psi, " .. sgResistanceDiff .. " ohms")
-        --print(voltageDiff)
         stressString = stressString .. ", " .. stress
       end
       file:write(stressString, "\n") -- Write data to file
-      --print(stressString) --print to console
+      print(stressString) --print to console
       
+      --zeroing
       if mbRead(2000, 0) < .5 then
         for i=1, table.getn(ainChannels) do --loop through channels
           ain = mbRead(ainChannels[i] * 2, 3) --get value
@@ -224,7 +176,7 @@ while true do --loop forever
   file:flush() --make sure its saved
   file:close() --close the file
   print("Closed file")
-  setSafeState(1)
+  mbWriteName("FIO1", 0) -- set led
 end
 
 
